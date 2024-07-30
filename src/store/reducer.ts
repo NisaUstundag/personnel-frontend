084b51bfcdb8ac577c1fc754c222885ea52f697c
Nisa Üstündağ <nisaustundag070@gmail.com>
2024-07-30 10:54:27 +0300

    Dosya isimleri düzenlendi

--- src/store/reducer.ts
+++ src/store/reducer.ts
@@ -1,29 +1,32 @@
-export interface Personnel {
-    id: number;
-    firstName: string;
-    lastName: string;
-    email: string;
-  }
-  
-  interface State {
-    personnel: Personnel[];
-  }
-  
-  export interface PersonnelAction {
-    type: 'ADD_PERSONNEL' | 'SET_PERSONNEL';
-    payload: Personnel | Personnel[];
-  }
-  
-  const personnelReducer = (state: State, action: PersonnelAction): State => {
-    switch (action.type) {
-      case 'ADD_PERSONNEL':
-        return { ...state, personnel: [...state.personnel, action.payload as Personnel] };
-      case 'SET_PERSONNEL':
-        return { ...state, personnel: action.payload as Personnel[] };
-      default:
-        return state;
-    }
-  };
-  
-  export default personnelReducer;
-  
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+
+interface Personnel {
+  id: number;
+  firstName: string;
+  lastName: string;
+  email: string;
+}
+
+interface PersonnelState {
+  personnel: Personnel[];
+}
+
+const initialState: PersonnelState = {
+  personnel: [],
+};
+
+const personnelSlice = createSlice({
+  name: 'personnel',
+  initialState,
+  reducers: {
+    addPersonnel: (state, action: PayloadAction<Personnel>) => {
+      state.personnel.push(action.payload);
+    },
+    setPersonnel: (state, action: PayloadAction<Personnel[]>) => {
+      state.personnel = action.payload;
+    },
+  },
+});
+
+export const { addPersonnel, setPersonnel } = personnelSlice.actions;
+export default personnelSlice.reducer;
